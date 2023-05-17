3c9a8f37f9a1ccab68283a3301a06138a6fe9f5b
Samantha Ahn <samanthaahn@Samanthas-MacBook-Pro.local>
2023-05-16 21:24:48 -0700

    added images and links

--- src/components/project.js
+++ src/components/project.js
@@ -1,12 +1,17 @@
 import React from 'react';
 
-function Project({ title }) {
+function Project({ title, image, link }) {
+  const handleProjectClick = () => {
+    window.open(link, '_blank');
+  };
+
   return (
-    <div className="project">
-      <div className="projectBox">
-      {title}
-    </div>
-    </div>
+    <a href={link} target="_blank" rel="noopener noreferrer" className="projectBox" onClick={handleProjectClick}>
+      <img src={image} alt={title} className="projectImage" />
+      <div className="projectContent">
+        <h2>{title}</h2>
+      </div>
+    </a>
   );
 }
 
--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import Project from '../components/project';
+import bookImage from '../assets/images/books.webp';
+import brainImage from '../assets/images/brain.jpeg';
+import desktopImage from '../assets/images/desktop.webp';
+import eyesImage from '../assets/images/eyes.jpeg';
+import pixelImage from '../assets/images/pixels.webp';
+import writingImage from '../assets/images/writing.jpeg';
+
 
 function Portfolio() {
   const projects = [
-    { id: 1, title: 'Lit List', link:'https://samanthaahn.github.io/fiesta-event/' },
-    { id: 2, title: 'Food Critics', link:'https://hidden-retreat-77670.herokuapp.com/'},
-    { id: 3, title: 'Tech Blog', link: 'https://gentle-chamber-77686.herokuapp.com/' },
-    { id: 4, title: 'Note Taker', link: 'https://github.com/samanthaahn/note-taker-starter-app' },
-    { id: 5, title: 'Weather API', link: 'https://samanthaahn.github.io/weather-information/' },
-    { id: 6, title: 'Work Day Scheduler', link:'https://samanthaahn.github.io/work-day-scheduler/' },
+    { id: 1, title: 'Lit List', link:'https://samanthaahn.github.io/fiesta-event/', image: bookImage },
+    { id: 2, title: 'Food Critics', link:'https://hidden-retreat-77670.herokuapp.com/', image: brainImage},
+    { id: 3, title: 'Tech Blog', link: 'https://gentle-chamber-77686.herokuapp.com/', image: writingImage },
+    { id: 4, title: 'Note Taker', link: 'https://github.com/samanthaahn/note-taker-starter-app', image: pixelImage },
+    { id: 5, title: 'Weather API', link: 'https://samanthaahn.github.io/weather-information/', image: eyesImage },
+    { id: 6, title: 'Work Day Scheduler', link:'https://samanthaahn.github.io/work-day-scheduler/', image: desktopImage },
   ];
 
   const handleProjectClick = (projectId) => {
